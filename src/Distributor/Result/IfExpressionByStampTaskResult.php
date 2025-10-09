2b70c4ef63099664f8cbfffc43a48cbf553b3be1
gs2-ci <ci@gs2.io>
2025-10-09 02:10:31 +0000

    1dcfb34620a7ae66060ada493e1d01fff0839a5d

--- src/Distributor/Result/IfExpressionByStampTaskResult.php
+++ src/Distributor/Result/IfExpressionByStampTaskResult.php
@@ -18,10 +18,44 @@
 namespace Gs2\Distributor\Result;
 
 use Gs2\Core\Model\IResult;
+use Gs2\Distributor\Model\VerifyActionResult;
+use Gs2\Distributor\Model\ConsumeActionResult;
+use Gs2\Distributor\Model\AcquireActionResult;
+use Gs2\Distributor\Model\TransactionResult;
 
 class IfExpressionByStampTaskResult implements IResult {
+    /** @var TransactionResult */
+    private $item;
+    /** @var bool */
+    private $expressionResult;
     /** @var string */
     private $newContextStack;
+
+	public function getItem(): ?TransactionResult {
+		return $this->item;
+	}
+
+	public function setItem(?TransactionResult $item) {
+		$this->item = $item;
+	}
+
+	public function withItem(?TransactionResult $item): IfExpressionByStampTaskResult {
+		$this->item = $item;
+		return $this;
+	}
+
+	public function getExpressionResult(): ?bool {
+		return $this->expressionResult;
+	}
+
+	public function setExpressionResult(?bool $expressionResult) {
+		$this->expressionResult = $expressionResult;
+	}
+
+	public function withExpressionResult(?bool $expressionResult): IfExpressionByStampTaskResult {
+		$this->expressionResult = $expressionResult;
+		return $this;
+	}
 
 	public function getNewContextStack(): ?string {
 		return $this->newContextStack;
@@ -41,11 +75,15 @@ class IfExpressionByStampTaskResult implements IResult {
             return null;
         }
         return (new IfExpressionByStampTaskResult())
+            ->withItem(array_key_exists('item', $data) && $data['item'] !== null ? TransactionResult::fromJson($data['item']) : null)
+            ->withExpressionResult(array_key_exists('expressionResult', $data) ? $data['expressionResult'] : null)
             ->withNewContextStack(array_key_exists('newContextStack', $data) && $data['newContextStack'] !== null ? $data['newContextStack'] : null);
     }
 
     public function toJson(): array {
         return array(
+            "item" => $this->getItem() !== null ? $this->getItem()->toJson() : null,
+            "expressionResult" => $this->getExpressionResult(),
             "newContextStack" => $this->getNewContextStack(),
         );
     }
--- src/Distributor/Result/IfExpressionByUserIdResult.php
+++ src/Distributor/Result/IfExpressionByUserIdResult.php
@@ -18,18 +18,56 @@
 namespace Gs2\Distributor\Result;
 
 use Gs2\Core\Model\IResult;
+use Gs2\Distributor\Model\VerifyActionResult;
+use Gs2\Distributor\Model\ConsumeActionResult;
+use Gs2\Distributor\Model\AcquireActionResult;
+use Gs2\Distributor\Model\TransactionResult;
 
 class IfExpressionByUserIdResult implements IResult {
+    /** @var TransactionResult */
+    private $item;
+    /** @var bool */
+    private $expressionResult;
+
+	public function getItem(): ?TransactionResult {
+		return $this->item;
+	}
+
+	public function setItem(?TransactionResult $item) {
+		$this->item = $item;
+	}
+
+	public function withItem(?TransactionResult $item): IfExpressionByUserIdResult {
+		$this->item = $item;
+		return $this;
+	}
+
+	public function getExpressionResult(): ?bool {
+		return $this->expressionResult;
+	}
+
+	public function setExpressionResult(?bool $expressionResult) {
+		$this->expressionResult = $expressionResult;
+	}
+
+	public function withExpressionResult(?bool $expressionResult): IfExpressionByUserIdResult {
+		$this->expressionResult = $expressionResult;
+		return $this;
+	}
 
     public static function fromJson(?array $data): ?IfExpressionByUserIdResult {
         if ($data === null) {
             return null;
         }
-        return (new IfExpressionByUserIdResult());
+        return (new IfExpressionByUserIdResult())
+            ->withItem(array_key_exists('item', $data) && $data['item'] !== null ? TransactionResult::fromJson($data['item']) : null)
+            ->withExpressionResult(array_key_exists('expressionResult', $data) ? $data['expressionResult'] : null);
     }
 
     public function toJson(): array {
         return array(
+            "item" => $this->getItem() !== null ? $this->getItem()->toJson() : null,
+            "expressionResult" => $this->getExpressionResult(),
         );
     }
 }
